cbfd8e4ee111a4df704c385a372c96b0e7e039e5
Alexandra <alexandra.sofronea27@gmail.com>
2022-05-05 21:34:29 +0300

    Basic structure commit

---
+++ src/main/java/examples/numberOne/NumberOneApplication.java
@@ -0,0 +1,13 @@
+package examples.numberOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/examples/numberOne/NumberOneApplicationTests.java
@@ -0,0 +1,13 @@
+package examples.numberOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
